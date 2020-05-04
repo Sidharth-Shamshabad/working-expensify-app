8b0a87b3cf9ff46eb5e124f4c614f1d85e25d3b0
Sidharth Shamshabad <sidharthshamshabad@Sidharths-MBP.T-mobile.com>
2020-05-04 18:08:42 -0400

    first commit with login page and layout pages completed

---
+++ src/components/CreateExamPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import {Link} from 'react-router-dom';
+
+const CreateExamPage = () => (
+    <div>
+        <h1>Create exam page</h1>
+        <button>Add Question</button>
+    </div>
+);
+
+export default CreateExamPage;
---
+++ src/components/TakeExamPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import {Link} from 'react-router-dom';
+
+const TakeExamPage = () => (
+    <div>
+        <h1>Available Exams</h1>
+    </div>
+);
+
+export default TakeExamPage;
