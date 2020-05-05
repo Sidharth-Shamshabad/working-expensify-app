a5a3653fe882b37f12c7db2b5263b241011cc52b
Sidharth Shamshabad <sid@techvoe.com>
2020-05-05 08:47:21 -0400

    edited back to final product

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -6,8 +6,7 @@ export const LoginPage = ({startLogin}) => (
     <div className="box-layout">
         <div className="box-layout__box">
             <h1 className="box-layout__title">Expensify</h1>
-            <p>A simple way to make and take exams.</p>
-            <p>* Remember to use Stony Brook Account! *</p>
+            <p>It's time to get your expenses under control.</p>
             <button className="button" onClick={startLogin}>Login with Google</button>
         </div>
     </div>
